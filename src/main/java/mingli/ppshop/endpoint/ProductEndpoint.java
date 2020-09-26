a604acd86f6ecff85ffdfce3e7ac8fe4e6867fe3
mingli <mingli2015md@gmail.com>
2020-09-27 00:36:31 +0200

    signup

--- src/main/java/mingli/ppshop/endpoint/ProductEndpoint.java
+++ src/main/java/mingli/ppshop/endpoint/ProductEndpoint.java
@@ -3,8 +3,11 @@ package mingli.ppshop.endpoint;
 import mingli.ppshop.models.AuthRequest;
 import mingli.ppshop.models.AuthResponse;
 import mingli.ppshop.PPUserDetailsService;
+import mingli.ppshop.models.User;
+import mingli.ppshop.repository.UserRepository;
 import mingli.ppshop.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -22,6 +25,9 @@ public class ProductEndpoint {
     private PPUserDetailsService ppUserDetailsService;
 
     @Autowired
+    UserRepository userRepository;
+
+    @Autowired
     private JwtUtil jwtUtil;
 
     @GetMapping("/hello")
@@ -31,17 +37,30 @@ public class ProductEndpoint {
 
     @PostMapping("/login")
     @CrossOrigin(origins = "*", maxAge = 3600)
-    public ResponseEntity<?> createAuthToken(@RequestBody AuthRequest authRequest) throws Exception {
+    public ResponseEntity<?> login(@RequestBody AuthRequest authRequest) {
         try {
             authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword())
             );
         } catch (BadCredentialsException e) {
-            throw new Exception("Incorrect username or password");
+            return new ResponseEntity<>("Incorrect username or password", HttpStatus.NOT_FOUND);
         }
         final UserDetails userDetails = ppUserDetailsService.loadUserByUsername(authRequest.getUsername());
         final String jwt = jwtUtil.generateToken(userDetails);
 
-        return ResponseEntity.ok(new AuthResponse(userDetails.getUsername(), jwt));
+        return new ResponseEntity<>(new AuthResponse(userDetails.getUsername(), jwt), HttpStatus.OK);
+    }
+
+    @PostMapping("/signup")
+    @CrossOrigin(origins = "*", maxAge = 3600)
+    public ResponseEntity<?> signup(@RequestBody AuthRequest authRequest) {
+
+        User created = userRepository.save(new User(authRequest.getUsername(), authRequest.getPassword(), true, "ROLE_USER"));
+
+        if (userRepository.existsById(created.getId())) {
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 }
--- src/main/java/mingli/ppshop/models/User.java
+++ src/main/java/mingli/ppshop/models/User.java
@@ -1,6 +1,7 @@
 package mingli.ppshop.models;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
@@ -11,6 +12,7 @@ import javax.persistence.Id;
 @Entity(name = "users")
 @Getter
 @Setter
+@NoArgsConstructor
 public class User {
 
     @Id
@@ -21,4 +23,10 @@ public class User {
     private boolean active;
     private String roles;
 
+    public User(String userName, String password, boolean active, String roles) {
+        this.userName = userName;
+        this.password = password;
+        this.active = active;
+        this.roles = roles;
+    }
 }
