a4b6c60fb49a13287a4f0867a876d54f995f881a
mingli <mingli2015md@gmail.com>
2020-09-30 15:32:57 +0200

    init data using script

--- src/main/java/mingli/ppshop/PPshopApplication.java
+++ src/main/java/mingli/ppshop/PPshopApplication.java
@@ -23,19 +23,4 @@ public class PPshopApplication {
         return new ModelMapper();
     }
 
-    @Bean
-    CommandLineRunner init(ProductRepository productRepository, UserRepository userRepository) {
-        Product product1 = new Product("Haribo 1", CATEGORY.A, "Happy Cola 200g", 0.97, 2, "https://onlineshop.haribo.com/media/image/e1/c0/b0/Happy-Cola-200-g_40016863151015b895bc20632e_260x260.png");
-        Product product2 = new Product("Haribo 2", CATEGORY.A, "Saft Gold 175g", 0.87, 2, "https://onlineshop.haribo.com/media/image/a9/dd/86/Saft_GB_175g-jpg_260x260.png");
-        Product product3 = new Product("Haribo 3", CATEGORY.A, "Pico Balla 175g", 0.88, 2, "https://onlineshop.haribo.com/media/image/55/83/b3/Pico-Balla-175-g_8426617106201_260x260.png");
-        User user = new User("ming@test", "password", true, "ROLE_USER");
-
-        return args -> {
-            productRepository.save(product1);
-            productRepository.save(product2);
-            productRepository.save(product3);
-            userRepository.save(user);
-        };
-    }
-
 }
